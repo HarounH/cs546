0a882803430f077b0f1dc9dbbb8e5758de63137d
Haroun H <haroun7@gmail.com>
2018-02-21 13:24:45 -0600

    get_data.py now fetches data from that repo

---
+++ data/get_data.py
@@ -0,0 +1,26 @@
+'''
+    File to get data for AES
+'''
+__author__ = 'Haroun Habeeb'
+__mail__ = 'haroun7@gmail.com'
+
+import os
+import pdb
+import urllib.request
+# Grabs ASAP data from https://github.com/nusnlp/nea/tree/master/data
+# The data itself is 5 fold, and each fold has a 60-20-20 (?) split.
+# This script downloads those individual files
+github_prefix = 'https://raw.githubusercontent.com/nusnlp/nea/master/data/'
+n_folds = 5
+filenames = ['dev_ids.txt', 'test_ids.txt', 'train_ids.txt']
+# Get each fold seperately
+for fold_idx in range(n_folds):
+    # make directory.
+    dir_name = 'fold_' + str(fold_idx)
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    for filename in filenames:
+        url = github_prefix + dir_name + '/' + filename
+        destination = dir_name + '/' + filename
+        urllib.request.urlretrieve(url, destination)
+        # pdb.set_trace()
