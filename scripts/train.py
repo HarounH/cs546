3fc1aabd7e97b69b61f2a00a3fd8fbc34191fb6d
Bhuvan Venkatesh <bhuvan.venkatesh21@gmail.com>
2018-02-24 17:35:05 -0600

    Got the configuration factored out

--- scripts/train.py
+++ scripts/train.py
@@ -6,18 +6,54 @@ import numpy as np
 import scipy
 from time import time
 import sys
-import src.utils as utils
+#import src.utils as utils
 import pickle as pk
+#from src.asap_evaluator import Evaluator
+#import src.asap_reader as dataset
+from jsonschema import validate
+import yaml
+
+# To refactor
+#from keras.preprocessing import sequence
 
 logger = logging.getLogger(__name__)
 
-###############################################################################################################################
-## Parse arguments
-#
+def parse_arguments():
+    """
+        Parses the config file and returns a dictionary
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", dest="config_file", type=str, 
+        metavar='<str>', required=True, help="The path configuration file")
+    parser.add_argument("-s", "--schema", dest="schema_file", type=str, 
+        metavar='<str>', required=True, default="schema.yaml", help="The path configuration file")
+    args = parser.parse_args()
+    return args
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--config", dest="config_file", type=str, metavar='<str>', required=True, help="The path configuration file")
-args = parser.parse_args()
+def validate_params(config_file: str, schema_file: str) -> dict:
+    """
+        Validates all the params int the config fields
+    """
+    config_file = yaml.load(open(config_file).read())
+    schema_file = yaml.load(open(schema_file).read())
+    validate(config_file, schema_file)
+    return config_file
+
+def set_logger(directory: str) -> None:
+
+
+def main() -> None:
+    args = parse_arguments()
+    configs = validate_params(args.config_file, args.schema_file)
+
+    np.random.seed(config['seed'])
+    print(configs)
+
+if __name__ == '__main__':
+    main()
+
+
+"""
 
 out_dir = args.out_dir_path
 
@@ -25,46 +61,29 @@ utils.mkdir_p(out_dir + '/preds')
 utils.set_logger(out_dir)
 utils.print_args(args)
 
-assert args.model_type in {'reg', 'regp', 'breg', 'bregp'}
-assert args.algorithm in {'rmsprop', 'sgd', 'adagrad', 'adadelta', 'adam', 'adamax'}
-assert args.loss in {'mse', 'mae'}
-assert args.recurrent_unit in {'lstm', 'gru', 'simple'}
-assert args.aggregation in {'mot', 'attsum', 'attmean'}
-
-if args.seed > 0:
-	np.random.seed(args.seed)
-
-if args.prompt_id:
-	from nea.asap_evaluator import Evaluator
-	import nea.asap_reader as dataset
-else:
-	raise NotImplementedError
-
 ###############################################################################################################################
 ## Prepare data
 #
 
-from keras.preprocessing import sequence
-
 # data_x is a list of lists
 (train_x, train_y, train_pmt), (dev_x, dev_y, dev_pmt), (test_x, test_y, test_pmt), vocab, vocab_size, overal_maxlen, num_outputs = dataset.get_data(
-	(args.train_path, args.dev_path, args.test_path), args.prompt_id, args.vocab_size, args.maxlen, tokenize_text=True, to_lower=True, sort_by_len=False, vocab_path=args.vocab_path)
+    (args.train_path, args.dev_path, args.test_path), args.prompt_id, args.vocab_size, args.maxlen, tokenize_text=True, to_lower=True, sort_by_len=False, vocab_path=args.vocab_path)
 
 # Dump vocab
 with open(out_dir + '/vocab.pkl', 'wb') as vocab_file:
-	pk.dump(vocab, vocab_file)
+    pk.dump(vocab, vocab_file)
 
 # Pad sequences for mini-batch processing
 if args.model_type in {'breg', 'bregp'}:
-	assert args.rnn_dim > 0
-	assert args.recurrent_unit == 'lstm'
-	train_x = sequence.pad_sequences(train_x, maxlen=overal_maxlen)
-	dev_x = sequence.pad_sequences(dev_x, maxlen=overal_maxlen)
-	test_x = sequence.pad_sequences(test_x, maxlen=overal_maxlen)
+    assert args.rnn_dim > 0
+    assert args.recurrent_unit == 'lstm'
+    train_x = sequence.pad_sequences(train_x, maxlen=overal_maxlen)
+    dev_x = sequence.pad_sequences(dev_x, maxlen=overal_maxlen)
+    test_x = sequence.pad_sequences(test_x, maxlen=overal_maxlen)
 else:
-	train_x = sequence.pad_sequences(train_x)
-	dev_x = sequence.pad_sequences(dev_x)
-	test_x = sequence.pad_sequences(test_x)
+    train_x = sequence.pad_sequences(train_x)
+    dev_x = sequence.pad_sequences(dev_x)
+    test_x = sequence.pad_sequences(test_x)
 
 ###############################################################################################################################
 ## Some statistics
@@ -77,14 +96,14 @@ dev_y = np.array(dev_y, dtype=K.floatx())
 test_y = np.array(test_y, dtype=K.floatx())
 
 if args.prompt_id:
-	train_pmt = np.array(train_pmt, dtype='int32')
-	dev_pmt = np.array(dev_pmt, dtype='int32')
-	test_pmt = np.array(test_pmt, dtype='int32')
+    train_pmt = np.array(train_pmt, dtype='int32')
+    dev_pmt = np.array(dev_pmt, dtype='int32')
+    test_pmt = np.array(test_pmt, dtype='int32')
 
 bincounts, mfs_list = utils.bincounts(train_y)
 with open('%s/bincounts.txt' % out_dir, 'w') as output_file:
-	for bincount in bincounts:
-		output_file.write(str(bincount) + '\n')
+    for bincount in bincounts:
+        output_file.write(str(bincount) + '\n')
 
 train_mean = train_y.mean(axis=0)
 train_std = train_y.std(axis=0)
@@ -129,11 +148,11 @@ optimizer = get_optimizer(args)
 from nea.models import create_model
 
 if args.loss == 'mse':
-	loss = 'mean_squared_error'
-	metric = 'mean_absolute_error'
+    loss = 'mean_squared_error'
+    metric = 'mean_absolute_error'
 else:
-	loss = 'mean_absolute_error'
-	metric = 'mean_squared_error'
+    loss = 'mean_absolute_error'
+    metric = 'mean_squared_error'
 
 model = create_model(args, train_y.mean(axis=0), overal_maxlen, vocab)
 model.compile(loss=loss, optimizer=optimizer, metrics=[metric])
@@ -152,9 +171,9 @@ plot(model, to_file = out_dir + '/model.png')
 
 logger.info('Saving model architecture')
 with open(out_dir + '/model_arch.json', 'w') as arch:
-	arch.write(model.to_json(indent=2))
+    arch.write(model.to_json(indent=2))
 logger.info('  Done')
-	
+    
 ###############################################################################################################################
 ## Evaluator
 #
@@ -173,24 +192,24 @@ total_train_time = 0
 total_eval_time = 0
 
 for ii in range(args.epochs):
-	# Training
-	t0 = time()
-	train_history = model.fit(train_x, train_y, batch_size=args.batch_size, nb_epoch=1, verbose=0)
-	tr_time = time() - t0
-	total_train_time += tr_time
-	
-	# Evaluate
-	t0 = time()
-	evl.evaluate(model, ii)
-	evl_time = time() - t0
-	total_eval_time += evl_time
-	
-	# Print information
-	train_loss = train_history.history['loss'][0]
-	train_metric = train_history.history[metric][0]
-	logger.info('Epoch %d, train: %is, evaluation: %is' % (ii, tr_time, evl_time))
-	logger.info('[Train] loss: %.4f, metric: %.4f' % (train_loss, train_metric))
-	evl.print_info()
+    # Training
+    t0 = time()
+    train_history = model.fit(train_x, train_y, batch_size=args.batch_size, nb_epoch=1, verbose=0)
+    tr_time = time() - t0
+    total_train_time += tr_time
+    
+    # Evaluate
+    t0 = time()
+    evl.evaluate(model, ii)
+    evl_time = time() - t0
+    total_eval_time += evl_time
+    
+    # Print information
+    train_loss = train_history.history['loss'][0]
+    train_metric = train_history.history[metric][0]
+    logger.info('Epoch %d, train: %is, evaluation: %is' % (ii, tr_time, evl_time))
+    logger.info('[Train] loss: %.4f, metric: %.4f' % (train_loss, train_metric))
+    evl.print_info()
 
 ###############################################################################################################################
 ## Summary of the results
@@ -202,4 +221,6 @@ logger.info('Evaluation: %i seconds in total' % total_eval_time)
 evl.print_final_info()
 
 if __name__ == '__main__':
-	# main()
+    # main()
+
+"""
