Fixed problems with the floating, not sure if the indexes are broadcasted correctly

original.py
@@ -77,9 +77,11 @@ def pad_sequences(vectorized_seqs, value=0.0, maxlen=None):
     # dump padding everywhere, and place seqs on the left.
     # NOTE: you only need a tensor as big as your longest sequence
     seq_tensor = Variable(torch.zeros((len(vectorized_seqs), maxlen)), requires_grad=False).long()
+    tensor_masque = Variable(torch.zeros((len(vectorized_seqs), maxlen)), requires_grad=False).long()
     for idx, (seq, seqlen) in enumerate(zip(vectorized_seqs, seq_lengths)):
         maxi = min(seqlen,maxlen)
         seq_tensor[idx, :maxi] = torch.LongTensor(seq[:maxi])
+        tensor_masque[idx, :maxi] = torch.ones((maxi,))
 
     # SORT YOUR TENSORS BY LENGTH!
     seq_lengths, perm_idx = seq_lengths.sort(0, descending=True)
@@ -87,7 +89,7 @@ def pad_sequences(vectorized_seqs, value=0.0, maxlen=None):
 
     seq_tensor = seq_tensor.transpose(0,1)
 
-    return seq_tensor, seq_lengths
+    return seq_tensor, seq_lengths, tensor_masque
 
 # data_x is a list of lists
 (train_x, train_y, train_pmt), (dev_x, dev_y, dev_pmt), (test_x, test_y, test_pmt), vocab, vocab_size, overal_maxlen, num_outputs = dataset.get_data(
@@ -134,10 +136,6 @@ test_mean = test_y.mean(axis=0)
 test_std = test_y.std(axis=0)
 
 logger.info('Statistics:')
-
-# logger.info('  train_x shape: ' + str(np.array(train_x).shape))
-# logger.info('  dev_x shape:   ' + str(np.array(dev_x).shape))
-# logger.info('  test_x shape:  ' + str(np.array(test_x).shape))
 
 logger.info('  train_y shape: ' + str(train_y.shape))
 logger.info('  dev_y shape:   ' + str(dev_y.shape))
@@ -204,39 +202,48 @@ test_pmt = from_numpy(test_pmt)
 
 def train(model, optimizer):
     model.train()
+    clipnorm = 10
     data, target = train_x_tuple, train_y
-    model.zero_grad()
     output = model(*data)
     optimizer.zero_grad()
     loss_value = loss(output, target)
-    loss_value.backward()
-    optimizer.step()
+    #loss_value.backward()
+    #torch.nn.utils.clip_grad_norm(model.parameters(), clipnorm)
+    #optimizer.step()
     return loss_value
 
+import time
+
+class Timer:    
+    def __enter__(self):
+        self.start = time.clock()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.clock()
+        self.interval = self.end - self.start
+
+    def __add__(self, x):
+        return self.interval + x
 
 for ii in range(args.epochs):
-    # TODO: Gradient clipping has to be done here instead of the optimizer
+    clipvalue = 0
 
     # Training
-    t0 = time()
-    calc_loss = train(model, optimizer)
-    tr_time = time() - t0
+    with Timer() as tr_time:
+        calc_loss = train(model, optimizer)
     total_train_time += tr_time
 
     # Evaluate
-    t0 = time()
+    with Timer() as evl_time:
+        pass
     #evl.evaluate(model, ii)
-    evl_time = time() - t0
     total_eval_time += evl_time
 
     # Print information
     logger.info('Epoch %d, train: %is, evaluation: %is' % (ii, tr_time, evl_time))
     logger.info('[Train] loss: %.4f' % (calc_loss,))
     #evl.print_info()
-
-###############################################################################################################################
-## Summary of the results
-#
 
 logger.info('Training:   %i seconds in total' % total_train_time)
 logger.info('Evaluation: %i seconds in total' % total_eval_time)

src/custom_layers.py
@@ -3,6 +3,7 @@ import sys
 import torch
 from torch.autograd import Variable
 from torch.nn.functional import softmax
+import functools
 
 class Attention(torch.nn.Module):
     def __init__(self, input_size, op='attsum', activation='tanh', init_stdev=0.01):
@@ -12,6 +13,7 @@ class Attention(torch.nn.Module):
         self.op = op
         self.activation = activation
         self.init_stdev = init_stdev
+        self.input_size = input_size
         self.att_v = Variable(torch.randn(input_size).mul(self.init_stdev), 
             requires_grad=True)
         self.att_W = Variable(torch.randn(input_size, input_size).mul(self.init_stdev), 
@@ -19,9 +21,11 @@ class Attention(torch.nn.Module):
 
     def tensordot(self, x, y):
         """
-        The last dimension of x and the first dimension of y have to match
+        x.size()[-1] == y.size()[0]
+        len(x.size()) > 1
+        len(y.size()) > 1
         """
-        mul = lambda lis: reduce(lambda x, y: x*y, lis, 1)
+        mul = lambda lis: functools.reduce(lambda x, y: x*y, lis, 1)
         x_shape = x.size()
         prev_dims = x_shape[:-1]
         concat_dim = x_shape[-1]
@@ -29,24 +33,20 @@ class Attention(torch.nn.Module):
         concat_dim_y = y_shape[0]
         after_dims = y_shape[1:]
         assert concat_dim == concat_dim_y
-        return torch.mm(x.resize_(mul(prev_dims),concat_dim), 
-            y.resize_(concat_dim_y, mul(after_dims))).resize_(prev_dims+after_dims)
+        return (x.view(mul(prev_dims),concat_dim).mm(y)).view(prev_dims + after_dims)
     
     def forward(self, x, mask=None):
         y = self.tensordot(x, self.att_W)
         if self.activation == 'tanh':
             y = torch.tanh(y)
-        print(self.att_v)
-        print(y)
-        weights = self.tensordot(y, self.att_v)
-        print(weights)
-        weights = softmax(weights)
-        out = x * K.permute_dimensions(K.repeat(weights, x.shape[2]), [0, 2, 1])
-
-        if self.op == 'attsum':
-            out = out.sum(axis=0)
-        elif self.op == 'attmean':
-            out = torch.addcdiv(torch.zeros(mask.size()), out.sum(dim=0), mask)
+        weights = self.tensordot(y, self.att_v.view((self.input_size, 1)))
+        weights = softmax(weights, dim=2)
+        repeated = torch.cat([weights] * self.input_size, dim=2)
+        out = x * repeated
+        out = out.sum(dim=0) # no need to incoporate the mas because 0 is the padding
+        if self.op == 'attmean':
+            adjusted_mask = torch.cat([mask.view(mask.size()[0], 1)] * x.shape[-1], dim=0).float()
+            out = out / Variable(adjusted_mask, requires_grad=False)
         return out.float()
 
 class MeanOverTime(torch.nn.Module):
@@ -54,10 +54,22 @@ class MeanOverTime(torch.nn.Module):
         super(MeanOverTime, self).__init__()
 
     def forward(self, x, mask=None):
-        if mask != None:
-            return torch.addcdiv(torch.zeros(mask.size()), x.sum(dim=0), mask)
+        if isinstance(mask, torch.LongTensor):
+            adjusted_mask = torch.cat([mask.view(mask.size()[0], 1)] * x.shape[-1], dim=1).float()
+            return x.sum(dim=0) / Variable(adjusted_mask, requires_grad=False)
         else:
             return x.mean(dim=0)
+
+class Conv1DWithMasking(torch.nn.Module):
+    def __init__(self, *args, **kwargs):
+        super(Conv1DWithMasking, self).__init__()
+        self.model = torch.nn.Conv1d(*args, **kwargs)
+    
+    def forward(self, x, mask=None):
+        x = self.model(x)
+        if isinstance(mask, torch.LongTensor):
+            x = x * mask
+        return x
 
 """
 class Attention(Layer):

src/models.py
@@ -4,7 +4,9 @@ import torch
 from torch.nn import Embedding
 from collections import OrderedDict
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-from .custom_layers import Attention, MeanOverTime
+from torch.autograd import Variable
+from .custom_layers import Attention, MeanOverTime, Conv1DWithMasking
+from .w2vEmbReader import W2VEmbReader as EmbReader
 
 logger = logging.getLogger(__name__)
 
@@ -12,43 +14,49 @@ dropout_W = 0.5
 dropout_U = 0.1
 cnn_border_mode='same'
 
-class UnidirectionalRegression(torch.nn.Module):
+class Regression(torch.nn.Module):
 
-    def __init__(self, vocab_size: int, 
-            embed_dim: int, cnn_dim: int, emb_path: str, 
-            rnn_dim: int, pooling: bool, dropout_prob: float, 
+    def __init__(self, vocab_size: int,
+            embed_dim: int, cnn_dim: int, emb_path: str,
+            rnn_dim: int, pooling: bool, dropout_prob: float,
             num_outputs: int, skip_init_bias: bool,
-            bias_value: float, aggregation: str):
+            bias_value: float, aggregation: str, bidirectional: bool,
+            vocab, cnn_window_size: int):
         """
         """
-        # TODO: Initialize the embeddings
-        super(UnidirectionalRegression, self).__init__()
-
+        super(Regression, self).__init__()
         self.embed = Embedding(vocab_size, embed_dim)
+        if emb_path:
+            logger.info('Initializing lookup table')
+            emb_reader = EmbReader(emb_path, emb_dim=emb_dim)
+            self.embed.W.data = emb_reader.get_emb_matrix_given_vocab(vocab)
+
         current_out_dim = embed_dim
         if cnn_dim > 0:
             # We may need to mess with this layer to get the masking right
-            self.conv_layer = torch.nn.Conv1d(current_out_dim,        # Input Dimension
+            self.conv_layer = Conv1DWithMasking(current_out_dim,        # Input Dimension
                         cnn_dim,                # Output Dimension
-                        stride=cnn_window_size) # TODO: subsample_length=1 Convert this
+                        cnn_window_size) # TODO: subsample_length=1 Convert this
             # We also need a kernel for this?
             current_out_dim = cnn_dim
 
         if rnn_dim > 0:
-            self.rnn_layer = torch.nn.RNN(input_size=current_out_dim, 
+            self.rnn_layer = torch.nn.RNN(input_size=current_out_dim,
                 hidden_size=rnn_dim,
                 num_layers=1, #???
                 nonlinearity='tanh', #???
-                dropout=dropout_W #???
+                dropout=dropout_W, #???
+                bidirectional=bidirectional
                 )
                 #return_sequences=pooling)
             current_out_dim = rnn_dim
+            if bidirectional:
+                current_out_dim *= 2
 
         layers = []
         if dropout_prob > 0:
             self.drop_layer = torch.nn.Dropout(p=dropout_prob)
 
-        self.pooling = pooling
         if pooling:
             if aggregation == 'mot':
                 self.agg = MeanOverTime()
@@ -63,66 +71,49 @@ class UnidirectionalRegression(torch.nn.Module):
         self.model = torch.nn.Sequential(OrderedDict(layers))
         self.embed_index = 0
 
-    def forward(self, x, mask):
+    def forward(self, x, mask, one_hot_mask):
         """
         """
         out = self.embed(x).float()
         if hasattr(self, 'conv_layer'):
-            out = self.conv_layer(out)
+            out = self.conv_layer(out, mask=one_hot_mask)
         if hasattr(self, 'rnn_layer'):
-            mask = mask.cpu().numpy()
-            packed_input = pack_padded_sequence(out, mask)
+            np_mask = mask.cpu().numpy()
+            packed_input = pack_padded_sequence(out, np_mask)
             packed_output, _ = self.rnn_layer(packed_input)
             out, _ = pad_packed_sequence(packed_output)
-            if self.pooling:
-                # Train with dropout over time
-                if hasattr(self, 'dropout'):
-                    out = self.dropout(out)
-                print(out)
-                out = self.agg(out, mask=mask)
-                print(out)
-                # Aggregate with MeanOverTime or Attention
-                # Feed to LinearLayer
-            else:
+            if not hasattr(self, 'agg'):
+                # If we don't pool, take the last timestamp
                 out = out[-1, :, :].float()
-                if hasattr(self, 'dropout'):
-                    out = self.dropout(out)
+
+        if hasattr(self, 'dropout'):
+            out = self.dropout(out)
+        if hasattr(self, 'agg'):
+            # We can have pooling without rnns, don't ask me how
+            out = self.agg(out, mask=mask)
 
         return self.model(out)
-
-class BidirectionalRegression(torch.nn.Module):
-
-    def __init__(self, vocab_size, 
-            embed_dim, cnn_dim, emb_path, rnn_dim, pooling: bool):
-        """
-        """
-        super(BidirectionalRegression, self).__init__()
-        self.embedding = Embedding(vocab_size, embed_dim) 
-
-
-    def forward(self, x):
-        """
-        """
-        return x
 
 def create_model(args, num_outputs, initial_mean_value, overal_maxlen, vocab):
     bias_value = (np.log(initial_mean_value) - np.log(1 - initial_mean_value))
     if args.model_type == 'reg':
         logger.info('Building a REGRESSION model')
-        return UnidirectionalRegression(args.vocab_size, args.emb_dim, 
-            args.cnn_dim, args.emb_path, 
-            args.rnn_dim, False, args.dropout_prob, 
-            num_outputs, args.skip_init_bias, 
-            bias_value, args.aggregation
-            )
+        return Regression(args.vocab_size, args.emb_dim,
+            args.cnn_dim, args.emb_path,
+            args.rnn_dim, False, args.dropout_prob,
+            num_outputs, args.skip_init_bias,
+            bias_value, args.aggregation, False, vocab,
+            args.cnn_window_size
+        )
     elif args.model_type == 'regp':
         logger.info('Building a REGRESSION model with POOLING')
-        return UnidirectionalRegression(args.vocab_size, args.emb_dim, 
-            args.cnn_dim, args.emb_path, 
-            args.rnn_dim, True, args.dropout_prob, 
-            num_outputs, args.skip_init_bias, 
-            bias_value, args.aggregation
-            )
+        return Regression(args.vocab_size, args.emb_dim,
+            args.cnn_dim, args.emb_path,
+            args.rnn_dim, True, args.dropout_prob,
+            num_outputs, args.skip_init_bias,
+            bias_value, args.aggregation, False, vocab,
+            args.cnn_window_size
+        )
         """
         model = Sequential()
         model.add(Embedding(args.vocab_size, args.emb_dim, mask_zero=True))
@@ -145,9 +136,17 @@ def create_model(args, num_outputs, initial_mean_value, overal_maxlen, vocab):
         model.add(Activation('sigmoid'))
         model.emb_index = 0
         """
-    
+
     elif args.model_type == 'breg':
         logger.info('Building a BIDIRECTIONAL REGRESSION model')
+        return Regression(args.vocab_size, args.emb_dim,
+            args.cnn_dim, args.emb_path,
+            args.rnn_dim, False, args.dropout_prob,
+            num_outputs, args.skip_init_bias,
+            bias_value, args.aggregation, True, vocab,
+            args.cnn_window_size
+        )
+        """
         model = Sequential()
         sequence = Input(shape=(overal_maxlen,), dtype='int32')
         output = Embedding(args.vocab_size, args.emb_dim, mask_zero=True)(sequence)
@@ -166,10 +165,18 @@ def create_model(args, num_outputs, initial_mean_value, overal_maxlen, vocab):
         score = Activation('sigmoid')(densed)
         model = Model(input=sequence, output=score)
         model.emb_index = 1
-    
+        """
+
     elif args.model_type == 'bregp':
         logger.info('Building a BIDIRECTIONAL REGRESSION model with POOLING')
-        from keras.layers import Dense, Dropout, Embedding, LSTM, Input, merge
+        return Regression(args.vocab_size, args.emb_dim,
+            args.cnn_dim, args.emb_path,
+            args.rnn_dim, True, args.dropout_prob,
+            num_outputs, args.skip_init_bias,
+            bias_value, args.aggregation, True, vocab,
+            args.cnn_window_size
+        )
+        """
         model = Sequential()
         sequence = Input(shape=(overal_maxlen,), dtype='int32')
         output = Embedding(args.vocab_size, args.emb_dim, mask_zero=True)(sequence)
@@ -190,18 +197,6 @@ def create_model(args, num_outputs, initial_mean_value, overal_maxlen, vocab):
         score = Activation('sigmoid')(densed)
         model = Model(input=sequence, output=score)
         model.emb_index = 1
-    
-    logger.info('  Done')
-    
-    ###############################################################################################################################
-    ## Initialize embeddings if requested
-    #
+        """
 
-    if args.emb_path:
-        from w2vEmbReader import W2VEmbReader as EmbReader
-        logger.info('Initializing lookup table')
-        emb_reader = EmbReader(args.emb_path, emb_dim=args.emb_dim)
-        model.layers[model.emb_index].W.set_value(emb_reader.get_emb_matrix_given_vocab(vocab))
-        logger.info('  Done')
-    
     return model

src/w2vEmbReader.py
@@ -4,7 +4,10 @@ import numpy as np
 
 logger = logging.getLogger(__name__)
 
-class W2VEmbReader:
+class W2VEmbReader(object):
+	"""
+	
+	"""
 	def __init__(self, emb_path, emb_dim=None):
 		logger.info('Loading embeddings from: ' + emb_path)
 		has_header=False
@@ -67,12 +70,8 @@ class W2VEmbReader:
 			if embedding:
 				ret.append(self.embeddings[word])
 		logger.info('%i/%i word vectors initialized (hit rate: %.2f%%)' % 
-			(len(embedding), len(vocab), (100.0*len(embedding))/len(vocab)))
+			(len(embedding), len(vocab), (100.0 * len(embedding)) / len(vocab)))
 		return np.array(ret)
 	
 	def get_emb_dim(self):
 		return self.emb_dim
-	
-	
-	
-	